5f6a62507c6e3017a38ab22f3d64e4ea35a308a3
Tolqinbek <ermatovtolqinbekuz@gmail.com>
2024-07-25 22:02:30 +0500

    config with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,34 @@ import { AppService } from './app.service';
 import { AutomobileModule } from './api/automobile/automobile.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Automobile } from './model/automobile.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AutoConfigService } from './common/config/config.service';
 
 @Module({
-  imports: [AutomobileModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'automobile',
-      entities: [Automobile],
-      synchronize: true,
+  imports: [
+    AutomobileModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DATABASE_HOST') || 'localhost',
+          port: 5432,
+          username: configService.get('DATABASE_USERNAME') || 'postgres',
+          password: configService.get('DATABASE_PASSWORD') || 'postgres',
+          database: configService.get('DATABASE_NAME') || 'automobile',
+          entities: [Automobile],
+          synchronize: true,
+        };
+      },
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AutoConfigService],
+  exports: [AutoConfigService],
 })
 export class AppModule {}
---
+++ src/common/config/config.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AutoConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get databaseHost(): string {
+    return this.configService.get<string>('DATABASE_HOST');
+  }
+
+  get databasePort(): number {
+    return this.configService.get<number>('DATABASE_PORT');
+  }
+
+  get databaseUsername(): string {
+    return this.configService.get<string>('DATABASE_USERNAME');
+  }
+  get databasePassword(): string {
+    return this.configService.get<string>('DATABASE_PASSWORD');
+  }
+  get databaseName(): string {
+    return this.configService.get<string>('DATABASE_NAME');
+  }
+  
+}
